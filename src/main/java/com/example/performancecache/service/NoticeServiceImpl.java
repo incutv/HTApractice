cc37f94e1df8e1a3bdec4f98233a87ab5fc2f668
rbska <rbska1234@naver.com>
2024-03-15 19:36:51 +0900

    인기글 조회 캐싱 처리완료

--- src/main/java/com/example/performancecache/service/NoticeServiceImpl.java
+++ src/main/java/com/example/performancecache/service/NoticeServiceImpl.java
@@ -2,16 +2,22 @@ package com.example.performancecache.service;
 
 import com.example.performancecache.dto.Notice;
 import com.example.performancecache.mapper.NoticeReadMapper;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
 
+
 @Slf4j
 @Service
+@Log4j2
 public class NoticeServiceImpl implements NoticeService{
 
     private NoticeReadMapper noticeReadMapper;
@@ -27,6 +33,7 @@ public class NoticeServiceImpl implements NoticeService{
 
 
     @Override
+    @Cacheable(value = "bestViewsCache")
     public List<Notice> getBestViews() {
 
         return noticeReadMapper.bestViews();
