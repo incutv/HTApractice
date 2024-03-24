ae3c0ae6b25c0018e41cf06a62dc1418acdd3bcb
Minju Ji <hinote444@gmail.com>
2024-03-25 03:50:46 +0900

    [style] 코드 포맷팅

--- src/main/java/com/example/performancecache/controller/DemoController.java
+++ src/main/java/com/example/performancecache/controller/DemoController.java
@@ -2,19 +2,20 @@ package com.example.performancecache.controller;
 
 import com.example.performancecache.dto.Notice;
 import com.example.performancecache.service.NoticeService;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/demo")
 public class DemoController {
     private NoticeService noticeService;
 
-    public DemoController(NoticeService noticeService){
-        this.noticeService=noticeService;
+    public DemoController(NoticeService noticeService) {
+        this.noticeService = noticeService;
     }
 
     @GetMapping("")
