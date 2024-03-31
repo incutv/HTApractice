1eab7a7cc75239848c1b18e17c412ce42b851adc
gi-dor <rltjs3563@naver.com>
2024-04-01 00:24:30 +0900

    Style :  무지성 테이블 만들기 shedlock

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE notice(
                        createDate DATETIME NOT NULL,
                        updateDate DATETIME NOT NULL
 );
+
+CREATE TABLE shedlock(
+    name VARCHAR(64),  --//job의 name PK로 활용
+    lock_until TIMESTAMP(3) NULL,  -- 2024-03-12T20:31:00.012189300 단위까지 체크 가능하도록 필드 타입 설정
+    locked_at TIMESTAMP(3) NULL,
+    locked_by VARCHAR(255), --// 작성자 정보
+    PRIMARY KEY (name)
+);
+
