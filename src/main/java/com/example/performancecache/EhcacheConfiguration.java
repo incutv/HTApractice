28f990d47e0770677cf521b5d82308277d6f952e
gi-dor <rltjs3563@naver.com>
2024-04-01 00:25:40 +0900

    Feat : @EnableSchedulerLock 추가

--- src/main/java/com/example/performancecache/EhcacheConfiguration.java
+++ src/main/java/com/example/performancecache/EhcacheConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.performancecache;
 
+import javax.sql.DataSource;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
@@ -8,9 +12,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @EnableCaching
+@EnableSchedulerLock(defaultLockAtLeastFor = "30s", defaultLockAtMostFor = "30s")
 public class EhcacheConfiguration {
 
     @Bean
@@ -26,4 +32,18 @@ public class EhcacheConfiguration {
         ehCacheManagerFactoryBean.setShared(true);
         return ehCacheManagerFactoryBean;
     }
+
+    // Config로 ShedLockConfig 를 생성
+    // ShedLockConfig 클래스 새로 만들었다가 경고문으로 어노테이션 사용못하게 해서
+    // 일단 여기에 만들었음
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime() // 서버 시간이 아닌 DB 시간을 사용
+                        .build()
+        );
+    }
+
 }
