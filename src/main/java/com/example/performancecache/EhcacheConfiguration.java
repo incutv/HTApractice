3e0f8d34c07396387f042e681558bf49e14b919e
gi-dor <rltjs3563@naver.com>
2024-04-02 23:30:59 +0900

    Style : LockProvider 이동

--- src/main/java/com/example/performancecache/EhcacheConfiguration.java
+++ src/main/java/com/example/performancecache/EhcacheConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @EnableCaching
-@EnableSchedulerLock(defaultLockAtLeastFor = "30s", defaultLockAtMostFor = "30s")
 public class EhcacheConfiguration {
 
     @Bean
@@ -33,17 +32,5 @@ public class EhcacheConfiguration {
         return ehCacheManagerFactoryBean;
     }
 
-    // Config로 ShedLockConfig 를 생성
-    // ShedLockConfig 클래스 새로 만들었다가 경고문으로 어노테이션 사용못하게 해서
-    // 일단 여기에 만들었음
-    @Bean
-    public LockProvider lockProvider(DataSource dataSource) {
-        return new JdbcTemplateLockProvider(
-                JdbcTemplateLockProvider.Configuration.builder()
-                        .withJdbcTemplate(new JdbcTemplate(dataSource))
-                        .usingDbTime() // 서버 시간이 아닌 DB 시간을 사용
-                        .build()
-        );
-    }
 
 }
