271c283c69958487536b2a922094e0e9c2d5098b
lucy <lucy1287@naver.com>
2024-04-03 10:06:15 +0900

    [FEAT] 비동기 메일 발송, 예외처리

--- src/main/java/com/example/performancecache/service/EmailService.java
+++ src/main/java/com/example/performancecache/service/EmailService.java
@@ -17,6 +17,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
@@ -35,20 +36,25 @@ public class EmailService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
 
-    @Scheduled(cron = "0 * * * * ?") // 매분마다 실행
+    @Scheduled(cron = "0 0 18 * * ?")
     @SchedulerLock(name = "ScheduledTask_run")
-    public void makeEmail() throws MessagingException {
-        List<Notice> bestViewedNotices = noticeService.getBestViewedNotices();
-        String title = "Top 10 Notice";
-        String content = bestViewedNotices.toString();
-        sendMail(setFrom, toMail, title, content);
-
-        System.out.println("task1: "+ LocalDateTime.now());
+    public void makeEmail() {
+        CompletableFuture.runAsync(() -> {
+            try {
+                List<Notice> bestViewedNotices = noticeService.getBestViewedNotices();
+                String title = "Top 10 Notice";
+                String content = bestViewedNotices.toString();
+                sendMail(setFrom, toMail, title, content);
+                System.out.println("task1: "+ LocalDateTime.now());
+            } catch (Exception e) {
+                LOGGER.error("오류: ", e.getMessage());
+            }
+        });
     }
 
-    @Scheduled(cron = "0 * * * * ?")
+    @Scheduled(cron = "0 0 18 * * ?")
     @SchedulerLock(name = "ScheduledTask_run")
-    public void makeEmail2() throws MessagingException {
+    public void makeEmail2() {
         List<Notice> bestViewedNotices = noticeService.getBestViewedNotices();
         String title = "Top 10 Notice";
         String content = bestViewedNotices.toString();
@@ -57,13 +63,17 @@ public class EmailService {
         System.out.println("task2: "+ LocalDateTime.now());
     }
 
-    private void sendMail(String setFrom, String toMail, String title, String content)  throws MessagingException {
-        MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-        helper.setFrom(setFrom);
-        helper.setTo(toMail);
-        helper.setSubject(title);
-        helper.setText(content);
-        emailSender.send(message);
+    private void sendMail(String setFrom, String toMail, String title, String content) {
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(setFrom);
+            helper.setTo(toMail);
+            helper.setSubject(title);
+            helper.setText(content);
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("이메일 발송 오류", e);
+        }
     }
 }
